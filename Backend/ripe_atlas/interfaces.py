c990df18f4bdb214588265a39cac26ad6e57cc6f
Sebastiaan <tientjieit@gmail.com>
2021-12-27 00:15:40 +0100

    added tests for ripe atlas interface and alert_configuration services.py

--- Backend/ripe_atlas/interfaces.py
+++ Backend/ripe_atlas/interfaces.py
@@ -1,6 +1,7 @@
 import requests
 from collections import defaultdict
 
+"""RIPE API URLS"""
 RIPE_BASE_URL = "https://atlas.ripe.net/api/v2/"
 MEASUREMENTS_URL = RIPE_BASE_URL + "measurements/"
 MY_MEASUREMENTS_URL = MEASUREMENTS_URL + "my/"
@@ -10,14 +11,14 @@ PROBES_URL = RIPE_BASE_URL + "probes/"
 RIPE_STATS_ASN_NEIGHBOURS = "https://stat.ripe.net/data/asn-neighbours/data.json"
 RIPE_STATS_ASN = "https://stat.ripe.net/data/as-overview/data.json"
 
-
+"""RIPE RESPONSE FIELDS"""
 # fields should be comma seperated for the fields query parameter, id and type is always included
 WANTED_PROBE_FIELDS = "id,is_anchor,type,address_v4,address_v6,asn_v4,asn_v6,geometry,prefix_v4,prefix_v6,description"
 WANTED_ANCHOR_FIELDS = "id,ip_v4,ipv6,as_v4,as_v6,geometry,prefix_v4,prefix_v6,fqdn"
 WANTED_ANCHOR_MEASUREMENT_FIELDS = "id,type,interval,description"
 WANTED_MEASUREMENT_FIELDS = "id,type,interval,description,target_ip,target,target_asn,target_prefix"
 
-# the type of  measurements that we can put an alert on
+"""SUPPORTED MEASUREMENTS"""
 SUPPORTED_TYPE_MEASUREMENTS = ('ping', 'traceroute')
 
 
@@ -25,7 +26,7 @@ class RipeInterface:
 
     @staticmethod
     def is_token_valid(token: str) -> bool:
-        response = requests.get(url=RIPE_BASE_URL + "credits/income-items", params={'key': token})
+        response = requests.get(url=CURRENT_PROBES_URL, params={'key': token})
         if response.status_code == 403:
             if response.json()['error']['detail'] == 'The provided API key does not exist':
                 return False
--- Backend/ripe_atlas/tests.py
+++ Backend/ripe_atlas/tests.py
@@ -1,13 +1,42 @@
 from django.test import TestCase
-from interfaces import RipeInterface
+from unittest.mock import Mock, patch
+from ripe_atlas.interfaces import RipeInterface
+
 """Integration tests Ripe Atlas"""
 
 
 class FirstTest(TestCase):
+
     def test_hello_world(self):
         text = "hello world!"
         self.assertEqual(text, "hello world!")
 
 
 class TestRipeInterface(TestCase):
-    pass
+
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_response_patcher = patch('ripe_atlas.interfaces.requests.get')
+        cls.mock_response = cls.mock_response_patcher.start()
+        cls.valid_token = "115c25fc-8815-4710-9865-85d49dc4778a"
+        cls.invalid_token = ""
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.mock_response_patcher.stop()
+
+    """
+    When we send a request to Ripe Atlas and we use a ripe_api_token, when the token is invalid we
+    get a 403 status code and if token is valid we get a 200 status code
+    """
+
+    def test_is_token_valid_when_valid(self):
+        self.mock_response.return_value.status_code = 200
+        valid = RipeInterface.is_token_valid(self.valid_token)
+        self.assertTrue(valid)
+
+    def test_is_token_valid_when_invalid(self):
+        self.mock_response.return_value.status_code = 403
+        invalid = RipeInterface.is_token_valid(self.invalid_token)
+        self.assertFalse(invalid)
+
