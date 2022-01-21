f27613dcfb4d50b43b7a9f86cfd51b2ca50a0657
Sebastiaan <tientjieit@gmail.com>
2022-01-21 10:15:24 +0100

    oude api verwijderd

--- AnomolyDetectionServer/simple_alert/apis.py
+++ AnomolyDetectionServer/simple_alert/apis.py
@@ -1,21 +1,10 @@
-from django.db import IntegrityError
-from rest_framework import permissions, status
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .monitors import Measurement
 from .monitor_manager import MonitorManager
 from .services import get_measurements
 
 monitor_manager = MonitorManager()
-
-
-class CreateMonitor(APIView):
-
-    def post(self, request):
-        measurement_id = request.data.get('measurement_id')
-        type = request.data.get('type')
-        monitor_manager.create_monitor(Measurement(measurement_id, type))
-        return Response(f"Measurement {measurement_id} is being monitored", status=status.HTTP_200_OK)
 
 
 class MonitorProcess(APIView):
@@ -35,9 +24,3 @@ class Feedback(APIView):
         monitor_id = request.data.get('id')
         monitor_manager.monitors[monitor_id].restart()
         return Response("Feedback has been processed", status=status.HTTP_200_OK)
-
-
-class Measurement(APIView):
-    def get(self, request):
-        return Response([measurement.measurement_id for measurement in get_measurements(208800)], status=status.HTTP_200_OK)
-
--- AnomolyDetectionServer/simple_alert/urls.py
+++ AnomolyDetectionServer/simple_alert/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
-from .apis import CreateMonitor, Feedback, MonitorProcess, Measurement
+from .apis import Feedback, MonitorProcess
 
 urlpatterns = [
     # path('', CreateMonitor.as_view()),
     path('', MonitorProcess.as_view()),
     path('feedback', Feedback.as_view()),
-    path('measurement', Measurement.as_view())
 ]
