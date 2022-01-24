72d1b6c5c9e4be77bb2562a0341d7f04343151a0
Sebastiaan <tientjieit@gmail.com>
2022-01-24 16:11:10 +0100

    fix db connection multiprocessing

--- AnomolyDetectionServer/simple_alert/monitor_manager.py
+++ AnomolyDetectionServer/simple_alert/monitor_manager.py
@@ -1,14 +1,14 @@
 from .monitors import Monitor
 from .monitor_strategies import PingMonitorStrategy, TracerouteMonitorStrategy
-from .models import Measurement, AlertConfiguration
-from typing import List
+from .models import AlertConfiguration
+from django import db
 
 
 class MonitorManager:
 
     def __init__(self):
         self.alert_configurations = AlertConfiguration.objects.all()
-
+        db.connections.close_all()
         self.monitors = dict()
         for alert_configuration in self.alert_configurations:
             if alert_configuration.measurement.type == 'Ping':
@@ -21,6 +21,7 @@ class MonitorManager:
             monitor.start()
 
     def create_monitor(self, alert_configuration: AlertConfiguration):
+        db.connections.close_all()
         if self.monitors.get(alert_configuration.alert_configuration_id) is None:
             if alert_configuration.measurement.type == 'Ping':
                 strategy = PingMonitorStrategy()
@@ -30,7 +31,6 @@ class MonitorManager:
             self.monitors[alert_configuration.alert_configuration_id].start()
 
     def restart_monitor(self, monitor_id):
-
         pass
 
     def train_monitor_model(self, monitor_id):
