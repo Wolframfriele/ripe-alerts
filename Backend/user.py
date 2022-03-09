232a9cbd65f031aeebf97e95aa05141f28ff2e60
koenb <koen.baltus@student.hu.nl>
2022-03-09 09:47:42 +0100

    Updated except

--- Backend/user.py
+++ Backend/user.py
@@ -3,4 +3,4 @@ import os
 try:
     os.system("python manage.py createsuperuser --noinput --username admin --email admin@myproject.com")
 except:
-    print("Can't create that superuser!")
+    pass
