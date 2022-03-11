2d661d20def6bc8cfe7e261f9c46a9abc6997dae
Wolframfriele <wolframfriele@gmail.com>
2022-03-11 13:21:37 +0100

    Build Info button component

---
+++ Frontend/src/components/InfoButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <q-btn class="info-button" round>?
+        <q-tooltip class="info-tooltip" anchor="center end" self="center start">{{ info }}</q-tooltip>
+    </q-btn>
+</template>
+
+<script>
+export default {
+     props: ['info']
+}
+</script>
+
+<style lang="css" scoped>
+.info-button {
+	float: right;
+}
+
+.info-tooltip {
+	max-width: 100px;
+}
+</style>
