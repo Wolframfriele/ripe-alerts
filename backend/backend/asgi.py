d2b7156d2d7177651aa0185da5fc492d31642f87
Floris Gravendeel <florisgra@gmail.com>
2022-03-22 13:16:06 +0100

    'backend' from scratch working in Docker

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RipeUgh project.
+ASGI config for Ripe-alert project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,11 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'notifications',
-    'ripe_atlas',
-    'users',
-    'alert_configuration',
+
 ]
 
 MIDDLEWARE = [
