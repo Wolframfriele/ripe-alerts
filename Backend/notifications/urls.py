09ebb9fed89b2faa07574d14bc01041bf3dea629
Floris Gravendeel <florisgra@gmail.com>
2022-03-22 12:56:06 +0100

    removed 'Backend'

--- Backend/notifications/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .apis import PluginConfigSystem, AlertSystem
-
-urlpatterns = [
-    path('config', PluginConfigSystem.as_view(), name="atlas-notification-config"),
-    path('publish', AlertSystem.as_view(), name="atlas-notification-broadcast")
-]
--- Backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .apis import UserDetail, RegistrationService, InitialSetup, ASNList
-
-urlpatterns = [
-    path('', UserDetail.as_view(), name='userdetail'),
-    path('registration-service', RegistrationService.as_view(), name="registration-service"),
-    path('initial-setup', InitialSetup.as_view(), name="initial-setup"),
-    path('monitored-asns', ASNList.as_view())
-]
