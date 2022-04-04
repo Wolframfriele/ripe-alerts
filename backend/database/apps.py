78c9e493cb6db09f8443119c624fc13229b1d16b
Floris Gravendeel <florisgra@gmail.com>
2022-04-04 15:00:07 +0200

    Added auto create admin (with if statement)

--- backend/database/apps.py
+++ backend/database/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class DatabaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'database'
+
+    def ready(self):
+        """To prevent: django.core.exceptions.AppRegistryNotReady-exception! Import after start-up!"""
+        from django.contrib.auth.models import User
+        admin_exist = User.objects.filter(username="admin").exists()
+        if not admin_exist:
+            User.objects.create_superuser(username="admin", email="admin@ripe.net", password="password")
+            print("Admin account created!")
