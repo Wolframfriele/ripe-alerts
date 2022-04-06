b0af1f06b6ef032b3f9dfc2180621bb2b8ed3cd5
jan <zeeslaghd@gmail.com>
2022-04-06 11:14:24 +0200

    add Apexcharts for Vue 3

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
+import VueApexCharts from "vue3-apexcharts";
 import axios from 'axios'
 
 axios.defaults.baseURL = "http://localhost:8000/api/"
@@ -10,4 +11,5 @@ axios.defaults.baseURL = "http://localhost:8000/api/"
 createApp(App)
 	.use(Quasar, quasarUserOptions)
 	.use(router)
+	.use(VueApexCharts)
 	.mount("#app");
