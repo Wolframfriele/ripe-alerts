f8d18f8acb5ad21445eb5db7737d4ed3bf086f6d
Floris Gravendeel <florisgra@gmail.com>
2022-04-08 11:35:28 +0200

    auto add settings for admin at startup

--- backend/database/apps.py
+++ backend/database/apps.py
@@ -9,11 +9,15 @@ class DatabaseConfig(AppConfig):
     def ready(self):
         """To prevent: django.core.exceptions.AppRegistryNotReady-exception! Import after start-up!"""
         from django.contrib.auth.models import User
+        from database.models import Setting
         auth_user_table_exist = "auth_user" in connection.introspection.table_names()
-        if not auth_user_table_exist:
+        if not auth_user_table_exist:  # Migrate before you create the user!
             return
         else:
             admin_exist = User.objects.filter(username="admin").exists()
             if not admin_exist:
-                User.objects.create_superuser(username="admin", email="admin@ripe.net", password="password")
+                user = User.objects.create_superuser(username="admin", email="admin@ripe.net", password="password")
                 print("Superuser 'admin' created!")
+            else:
+                user = User.objects.get(username="admin")
+            Setting.objects.get_or_create(user=user)
