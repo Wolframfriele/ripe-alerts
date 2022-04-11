0d6c82941c05cfb4112f822b3d24442b291e1ce0
Floris Gravendeel <florisgra@gmail.com>
2022-04-11 16:28:08 +0200

    added tests

--- backend/ripe_interface/tests.py
+++ backend/ripe_interface/tests.py
@@ -6,36 +6,54 @@ from django.urls import reverse
 
 # Valid ASN:12770, ASN:29432
 from ripe_interface.anchor import Anchor
-from ripe_interface.requests import ANCHORS_URL
+from ripe_interface.requests import ANCHORS_URL, RipeRequests
 
 
-class ASTestCase(TestCase):
-    lion = 1
-    cat = 22
-    client = Client()
+class RipeRequestTest(TestCase):
 
     def setUp(self):
-        self.cat = 223  # cat is now another number
+        self.invalid_asn = 0   # This will always be an invalid Autonomous System Number.
+        self.valid_asn = 1103  # A valid ASN with multiple anchors.
+        self.invalid_target_address = "194.171.96.34"  # Target address of ASN1104 (which consists of probes only)
+        self.valid_target_address = "195.169.125.10"  # Target address of ASN1103
 
-    def test_animals_can_speak(self):
-        """Autonomous systems that can be added or deleted"""
-        self.assertNotEquals(self.lion, 12)
-        self.assertNotEquals(self.cat, 22)
+    def test_get_anchors_invalid(self):
+        total_anchors = len(RipeRequests.get_anchors(self.invalid_asn))
+        self.assertEqual(total_anchors, 0)
 
-    def test_get_random_autonomous_system(self):  # -> list[Anchor]:
-        """Returns a list of random currently online anchors """
-        random_id = random.randint(1, 30)
-        # print(random_id)
-        response = requests.get(url=ANCHORS_URL + "10").json()
-        anchor = Anchor(**response)
-        print(anchor)
+    def test_get_anchors_valid(self):
+        anchors = RipeRequests.get_anchors(self.valid_asn)
+        total_anchors = len(anchors)
+        self.assertNotEquals(total_anchors, 0)
 
+    def test_autonomous_system_exist(self):
+        self.assertEqual(RipeRequests.autonomous_system_exist(self.invalid_asn), False)
+        self.assertEqual(RipeRequests.autonomous_system_exist(self.valid_asn), True)
 
-        self.assertEqual(1, 1)
+    def test_get_anchoring_measurements_invalid(self):
+        total_measurements = len(RipeRequests.get_anchoring_measurements(self.invalid_target_address))
+        self.assertEqual(total_measurements, 0)
 
+    def test_get_anchoring_measurements_valid(self):
+        total_measurements = len(RipeRequests.get_anchoring_measurements(self.valid_target_address))
+        self.assertNotEquals(total_measurements, 0)
 
 class GetSingleASTest(TestCase):
     """ Test module for GET single puppy API """
+
+    # def test_get_random_autonomous_system(self):  # -> list[Anchor]:
+    #     """Returns a list of random currently online anchors """
+    #     random_id = random.randint(1, 30)
+    #     # print(random_id)
+    #     response = requests.get(url=ANCHORS_URL + "10").json()
+    #     anchor = Anchor(**response)
+    #     print(anchor)
+    #     self.assertEqual(1, 1)
+    #
+    # def test_set_asn_endpoint(self):
+    #     self.assertEqual(1, 1)
+
+    # client = Client()
 
     # def setUp(self):
     #     return
