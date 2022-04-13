9a60070018aecf8339a8936b2db45fe77a68ea81
Floris Gravendeel <florisgra@gmail.com>
2022-04-13 09:47:34 +0200

    CRUD bugfix

--- backend/anomaly_detection/apps.py
+++ backend/anomaly_detection/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SimpleAlertConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'anomaly_detection'
+
+    # def ready(self):
+    #     monitor_manager = MonitorManager()
+
