c6ded33d74986b08f3b35822280249362b8836bf
Floris Gravendeel <florisgra@gmail.com>
2022-04-21 17:55:04 +0200

    sqlite3 default for testing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,7 +98,13 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 }
-
+# Use sqlite3 for testing.
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
