77baf57b02a86f99bb1821569fdf21055a7493b4
Floris Gravendeel <florisgra@gmail.com>
2022-04-26 13:54:38 +0200

    added auth bool for Django Ninja

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,22 +20,38 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from ninja.security import django_auth
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.views.generic import RedirectView
 from ninja import NinjaAPI
+
+from backend import settings
 from ripe_interface.api import router as ripe_interface_router
-
-
-description = "Welcome to our backend server!<br></br>Go to: " \
-              "<a href='/admin/'>Django administration panel</a>"
-
-api = NinjaAPI(title="Ripe Alerts API", version="0.1", description=description, csrf=False)
-api.add_router("/asn/", ripe_interface_router)
 
 
 def api_redirect(request):
     return redirect('/api/docs#/')
 
+
+def auth_configuration():
+    if settings.NINJA_AUTH_ENABLED:
+        return django_auth
+    elif not settings.NINJA_AUTH_ENABLED:
+        return None
+
+
+description = ""
+if settings.NINJA_AUTH_ENABLED:
+    description = "Welcome to our backend server!<br><br>Authentication is enabled. To change this, " \
+                  "open <u>settings.py</u> and set NINJA_AUTH_ENABLED to false.  <br></br>Go to: " \
+                  "<a href='/admin/'>Django administration panel</a>"
+elif not settings.NINJA_AUTH_ENABLED:
+    description = "Welcome to our backend server!<br><br>Authentication is disabled. To change this, " \
+                  "open <u>settings.py</u> and set NINJA_AUTH_ENABLED to true.  <br></br>Go to: " \
+                  "<a href='/admin/'>Django administration panel</a>"
+
+api = NinjaAPI(title="Ripe Alerts API", version="0.1", description=description, csrf=True)
+api.add_router("/asn/", ripe_interface_router, auth=auth_configuration())
 
 urlpatterns = [
     path('admin/', admin.site.urls),
