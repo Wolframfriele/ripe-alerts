4120e49678e23a6ee269f5d0eee41a2bd7c744a4
Floris Gravendeel <florisgra@gmail.com>
2022-04-26 15:31:25 +0200

    added feedback not working tho

--- backend/ripe_interface/api_schemas.py
+++ backend/ripe_interface/api_schemas.py
@@ -3,7 +3,7 @@ from typing import Optional
 from ninja import Schema
 from pydantic import Field
 
-from database.models import MeasurementType
+from database.models import MeasurementType, Feedback
 
 
 class AutonomousSystemSetting(Schema):
@@ -38,20 +38,31 @@ class AnomalyOut(Schema):
     timestamp: str = Field("2022-4-13 8:41:28",
                            description="Date and time when the anomaly occurred. Format: yyyy-mm-dd "
                                        "hh:mm:ss")
-    ip_address: str = Field("localhost", description="Target address of Autonomous System")
-    # autonomous_system: AutonomousSystem
+    ip_address: list[str] = Field("localhost", description="Target address of Autonomous System")
     description: str = Field("Ping above 200ms for 5 minutes, normally 12ms.", description="Explanation of the anomaly")
     measurement_type: MeasurementType
     detection_method: DetectionMethodOut
-    medium_value: float = Field(40.122, description="Average increase of round trip time (ms) at time of anomaly.")
-    value: float = Field(12.144, description="Average RTT (ms) at time of anomaly")
+    mean_increase: float = Field(40.122, description="Average increase of round trip time (ms) at time of anomaly.")
     anomaly_score: float = Field(22,
                                  description="The strength of the anomaly? Percentage of probes that report an anomaly.")
     prediction_value: bool = Field(True,
-                                   description="User feedback, whether the prediction was accurate (True) or not. (False)")
-    asn_error: int = Field(1402, description="ASN of the router were the error happened.")
+                                   description="Prediction by the Feedback Engine, "
+                                               "whether the prediction was accurate (True) or not. (False)")
+    asn: int = Field(1402, description="ASN of the router were the error happened.")
+    feedback: Optional[bool] = Field(None, description="User feedback, whether the prediction was accurate (True) or "
+                                              "not. (False)")
 
     @staticmethod
     def resolve_timestamp(obj):
         return str(obj.time.year) + "-" + str(obj.time.month) + "-" + str(obj.time.day) + \
                " " + str(obj.time.hour) + ":" + str(obj.time.minute) + ":" + str(obj.time.second)
+
+    @staticmethod
+    def resolve_ip_address(obj):
+        return str(obj.ip_address).replace(' ', '').split(",")
+
+    @staticmethod
+    def resolve_feedback(obj):
+        # print(obj.autonomous_system_id)
+        # print(type(obj.autonomous_system_id))
+        return Feedback.get_feedback(obj.autonomous_system_id)
