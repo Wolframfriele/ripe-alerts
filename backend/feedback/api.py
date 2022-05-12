eab227008d0d661ac1e3212d3d65beb337988eee
Floris Gravendeel <florisgra@gmail.com>
2022-05-12 10:39:45 +0200

    saving feedback bug fixed

--- backend/feedback/api.py
+++ backend/feedback/api.py
@@ -1,23 +1,21 @@
 from ssl import create_default_context
 from turtle import up
 from urllib import response
-from ninja import Router, Path
-from database.models import Feedback
+
+from django.http import JsonResponse
+from ninja import Router, Path, Query
+from database.models import Feedback, Anomaly
 from feedback.api_schema import FeedbackFormat, FeedbackOut
 
 router = Router()
 """Tags are used by Swagger to group endpoints."""
 TAG = "Feedback"
 
-@router.put("/feedback", response=FeedbackOut, tags=[TAG]) #TODO: Create documentation
-def save_feedback(request, data: FeedbackFormat = Path(...)):
-    print(data)
+
+@router.put("/feedback", response=FeedbackOut, tags=[TAG])  # TODO: Create documentation
+def save_feedback(request, data: FeedbackFormat = Query(...)):
+    anomaly_exist = Anomaly.objects.filter(id=data.anomaly_id).exists()
+    if not anomaly_exist:
+        return JsonResponse({"message": "Failed, anomaly bla bla does not exist!"}, status=404)
     Feedback.create_or_update(data.anomaly_id, data.user_feedback)
-    # feedback, created = Feedback.objects.get_or_create(anomaly_id=data.anomaly_id, response=data.user_feedback)
-    # print(feedback)
-    # print(created)
-    # if not created:
-    #     print('updating data')
-    #     feedback = Feedback.objects.filter(anomaly_id=data.anomaly_id).update(response=data.user_feedback)
-    #     print(feedback)
-    return "Feedback has been saved succesfully!"
+    return JsonResponse({"message": "Feedback has been saved successfully!"}, status=200)
