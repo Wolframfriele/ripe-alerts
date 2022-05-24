35c0f6d31cc1afe3ec4ec21796e803c4ca346d54
Floris Gravendeel <florisgra@gmail.com>
2022-05-24 14:15:21 +0200

    added anomaly startup script

--- backend/anomaly_detection_reworked/apps.py
+++ backend/anomaly_detection_reworked/apps.py
@@ -1,5 +1,13 @@
+import threading
 
 from django.apps import AppConfig
+
+from anomaly_detection_reworked.measurement_result_stream import MeasurementResultStream
+
+
+def start_anomaly_detection():
+    # threading.Thread(target=MeasurementResultStream).start()
+    print("Started Anomaly Detection!")
 
 
 class AnomalyDetectionConfig(AppConfig):
@@ -7,9 +15,20 @@ class AnomalyDetectionConfig(AppConfig):
     name = 'anomaly_detection_reworked'
 
     def ready(self):
+        """ To prevent Django for starting Anomaly Detection twice! """
         import os
         run_once = os.environ.get('CMDLINERUNNER_RUN_ONCE')
         if run_once is not None:
             return
         os.environ['CMDLINERUNNER_RUN_ONCE'] = 'True'
-        print("Anomaly Detection started")
+
+        import sys
+        if sys.argv is None:
+            return
+        for argument in sys.argv:
+            if argument == '--noreload':
+                start_anomaly_detection()
+                return
+        print("Anomaly Detection disabled.")
+        print("To enable this, please use the --noreload option at "
+              "startup.")
