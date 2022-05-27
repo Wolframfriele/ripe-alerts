10a6641105aaf86da17068a434ffd08b0cc02764
Floris Gravendeel <florisgra@gmail.com>
2022-05-27 21:51:08 +0200

    start of anomaly detection

---
+++ backend/anomaly_detection_reworked/anomaly_detection.py
@@ -0,0 +1,11 @@
+from anomaly_detection_reworked.detection_method import DetectionMethod
+
+
+class AnomalyDetection:
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def add_detection_method(self, method: DetectionMethod):
+        pass
+
---
+++ backend/anomaly_detection_reworked/detection_method.py
@@ -0,0 +1,9 @@
+from anomaly_detection_reworked.anomaly_detection import AnomalyDetection
+
+
+class DetectionMethod:
+    pass
+
+
+def set_anomaly_detection_instance(instance: AnomalyDetection): # Dependency Injection
+    pass
